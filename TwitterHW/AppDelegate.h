173c7994ba5d455b1414ceef6a215972293a1796
nathanspeller <speller.nathan@gmail.com>
2014-03-27 12:32:19 -0700

    Initial Commit

---
+++ TwitterHW/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterHW
+//
+//  Created by Nathan Speller on 3/27/14.
+//  Copyright (c) 2014 Nathan Speller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
